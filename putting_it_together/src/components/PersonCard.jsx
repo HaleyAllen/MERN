cdae64efebce4ee805e26a2a006d3c24f7399378
HaleyAllen <haleyallen2074@gmail.com>
2022-01-16 15:34:18 -0800

    inital commit

--- putting_it_together/src/components/PersonCard.jsx
+++ putting_it_together/src/components/PersonCard.jsx
@@ -1,8 +1,24 @@
 import React, {Component} from "react";
 
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            age: this.props.age
+        }
+    }
+
+    ageHandler =() =>{
+        this.setState({
+            age: this.state.age +1
+        })
+    }
+
     render(){
-        return(<><h1>{this.props.lastName}, {this.props.firstName}</h1><p>Age: {this.props.age}</p> <p>Hair Color: { this.props.hairColor }</p></>)
+        return(<><h1>{this.props.lastName}, {this.props.firstName}</h1>
+        <p>Age: {this.state.age}</p>
+        <p>Hair Color: { this.props.hairColor }</p>
+        <button onClick={this.ageHandler}>Birthday Button For { this.props.firstName }{ this.props.lastName }</button></>)
     }
 }
 export default PersonCard
